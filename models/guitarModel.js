b982841c70bfb2576ca6db042bc435b4593c68bf
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-12 23:07:33 +0100

    MongoDB model implemented into project. Schema created which requires some refinement. HTTP methods working correctly.

---
+++ models/guitarModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const guitarSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A guitar requires a name'],
+    unique: true,
+  },
+  manufacturer: {
+    type: String,
+    required: [true, 'A guitar requires a manufacturer'],
+  },
+  introduced: {
+    type: Number,
+    required: [true, 'A guitar requires a year of introduction'],
+  },
+  neck: {
+    type: String,
+  },
+  fretboard: {
+    type: String,
+  },
+  body: {
+    type: String,
+  },
+  scale: {
+    type: Number,
+  },
+  bridge: {
+    type: String,
+  },
+  pickups: {
+    type: String,
+  },
+  desc: {
+    type: String,
+    default: 'Description',
+    required: [true, 'A guitar requires a description'],
+  },
+  img: {
+    type: String,
+  },
+});
+
+const Guitar = mongoose.model('Guitar', guitarSchema);
+module.exports = Guitar;
