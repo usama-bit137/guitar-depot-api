0645393cdcbe60bcac4de825f2c026071b3c6efb
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-12 21:04:42 +0100

    index.js handles the templates and replaces templates with required data from devdata/data.json. Non-functional at the moment.

--- index.js
+++ index.js
@@ -1,50 +1,64 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
-const replaceTemplate =  require("./modules/replaceTemplate");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
 
-const guitar = fs.readFileSync(`${__dirname}/devdata/data.json`, "utf-8");
+const replaceTemplate = require('./modules/replaceTemplate');
+
+const guitar = fs.readFileSync(`${__dirname}/devdata/data.json`, 'utf-8');
 const dataObj = JSON.parse(guitar);
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
-const tempStack = fs.readFileSync(`${__dirname}/templates/template-stack.html`, "utf-8");
-const tempGuitar = fs.readFileSync(`${__dirname}/templates/template-guitar.html`, "utf-8")
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+
+const tempStack = fs.readFileSync(
+  `${__dirname}/templates/template-stack.html`,
+  'utf-8'
+);
+const tempGuitar = fs.readFileSync(
+  `${__dirname}/templates/template-guitar.html`,
+  'utf-8'
+);
+
+const slugs = dataObj.map((item) =>
+  slugify(`${item.manufacturer} ${item.name}`, { lower: true })
+);
+
+// console.log(slugs);
 
 const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+  if (pathname === '/' || pathname === '/overview') {
+    const guitarHTML = dataObj
+      .map((item) => replaceTemplate(tempStack, item))
+      .join('');
+    const output = tempOverview.replace('{%GUITAR_CARDS%}', guitarHTML);
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end(output);
+  } else if (pathname === '/guitar') {
+    const guitar = dataObj[query.id];
+    const output = replaceTemplate(tempGuitar, guitar);
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    res.end(output);
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(guitar);
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+      header: 'API',
+    });
+    res.end('<h1>404 error. This page could not be found!</h1>');
+  }
+});
 
-    const { query, pathname } = url.parse(req.url, true);
-    if (pathname === "/" || pathname === "/overview"){
-
-        const guitarHTML = dataObj.map(item => replaceTemplate(tempStack, item)).join("");
-        const output = tempOverview.replace("{%GUITAR_CARDS%}", guitarHTML); 
-        res.writeHead(200, {
-            "Content-type": "text/html",
-            "my-own-header": "hello-world"
-        });
-        res.end(output);
-
-    } else if (pathname === "/guitar") {
-
-        const guitar = dataObj[query.id];
-        const output = replaceTemplate(tempGuitar, guitar);
-        res.writeHead(200, {"Content-type": "text/html"});
-        res.end(output);
-    
-    } else if (pathname === "/api") {
-    
-        res.writeHead(200, {"Content-Type": "application/json"});
-        res.end(guitar);
-    
-    } else {
-    
-        res.writeHead(404, {
-            "Content-Type": "text/html",
-            "header": "API",
-        })
-        res.end("<h1>404 error. This page could not be found!</h1>")
-          
-    }
-})
-
-server.listen(8000, "127.0.0.1", () => {
-    console.log("Listening to requests on port 8000");
-})
+const port = 8000;
+server.listen(port, '127.0.0.1', () => {
+  console.log(`Listening to requests on port ${port}`);
+});
