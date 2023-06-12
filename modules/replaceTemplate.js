e851218e8b2e5e126ea5fad6812cf5a211fb311a
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-12 21:08:10 +0100

    This file replaces the template html files with the requested response data from the server. currently non-functional.

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,17 +1,17 @@
 module.exports = (temp, guitar) => {
-    let output = temp.replace(/{%IMG%}/g, guitar.img);
-    output = output.replace(/{%MANUFACTURER%}/g, guitar.manufacturer)
-    output = output.replace(/{%NAME%}/g, guitar.name)
-    output = output.replace(/{%FRETBOARD%}/g, guitar.introduced)
-    output = output.replace(/{%INTRODUCED%}/g, guitar.introduced)
-    output = output.replace(/{%BODY%}/g, guitar.body)
-    
-    output = output.replace(/{%NECK%}/g, guitar.neck)
-    output = output.replace(/{%SCALE%}/g, guitar.scale)
-    output = output.replace(/{%BRIDGE%}/g, guitar.bridge)
-    output = output.replace(/{%PICKUPS%}/g, guitar.pickups)
-    
-    output = output.replace(/{%DESCRIPTION%}/g, guitar.desc)
-    output = output.replace(/{%ID%}/g, guitar.id) 
-    return output
-}
+  let output = temp.replace(/{%IMG%}/g, guitar.img);
+  output = output.replace(/{%MANUFACTURER%}/g, guitar.manufacturer);
+  output = output.replace(/{%NAME%}/g, guitar.name);
+  output = output.replace(/{%FRETBOARD%}/g, guitar.introduced);
+  output = output.replace(/{%INTRODUCED%}/g, guitar.introduced);
+  output = output.replace(/{%BODY%}/g, guitar.body);
+
+  output = output.replace(/{%NECK%}/g, guitar.neck);
+  output = output.replace(/{%SCALE%}/g, guitar.scale);
+  output = output.replace(/{%BRIDGE%}/g, guitar.bridge);
+  output = output.replace(/{%PICKUPS%}/g, guitar.pickups);
+
+  output = output.replace(/{%DESCRIPTION%}/g, guitar.desc);
+  output = output.replace(/{%ID%}/g, guitar.id);
+  return output;
+};
