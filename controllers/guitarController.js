ea2a0426137e86cf9bc958d88bb1f5fc76a2a9a5
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-13 23:42:27 +0100

    Imported APIFeatures class into controller module. Code much cleaner using modular structure.

--- controllers/guitarController.js
+++ controllers/guitarController.js
@@ -1,54 +1,14 @@
 const Guitar = require(`../models/guitarModel`);
+const APIFeatures = require('../utils/APIFeatures');
+
 exports.getAllGuitars = async (req, res) => {
   try {
-    // BUILD QUERY
-    // 1A) Filtering:
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advanced Filtering:
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    // EXECUTE QUERY
-    let query = Guitar.find(JSON.parse(queryStr));
-
-    // 2) SORTING:
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('introduced');
-    }
-    // sorting completed
-
-    // 3) limiting fields
-    // limiting fields is the idea of only showing the
-    // fields requested by the user in the url
-
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination:
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-    // SEND RESPONSE
-
-    if (req.query.page) {
-      const numGuitars = await Guitar.countDocuments();
-      if (skip > numGuitars) throw new Error('This page does not exist');
-    }
-
-    const guitars = await query;
+    const features = new APIFeatures(Guitar.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const guitars = await features.query;
 
     res.status(200).json({
       status: 'success',
