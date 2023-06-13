657266040a777b42410c1df8e054559f5ae6f0bd
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-13 13:05:48 +0100

    No changes -- file configuration altered in .prettierrc.

--- routes/guitarRoutes.js
+++ routes/guitarRoutes.js
@@ -3,10 +3,7 @@ const express = require('express');
 const controller = require('../controllers/guitarController');
 const router = express.Router();
 
-router
-  .route('/')
-  .get(controller.getAllGuitars)
-  .post(controller.createGuitar);
+router.route('/').get(controller.getAllGuitars).post(controller.createGuitar);
 
 router
   .route('/:id')
