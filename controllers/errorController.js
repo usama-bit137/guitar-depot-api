0afd0ec3d3e5d2cfa4abf91db70a171479db4e30
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-17 22:13:29 +0100

    MongoDB Cast error implemented in the developer build.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,9 @@
-// const AppError = require('../utils/AppError');
+const AppError = require('../utils/AppError');
+
+const handleCastErrorDB = (err, res) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -28,6 +33,8 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
