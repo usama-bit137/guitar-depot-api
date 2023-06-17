32862afc5db99d92bce8d06f85e644f831db6b02
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-17 02:24:42 +0100

    Added development and production build errors. This makes the errors more informative development and user-friendly in production build.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,5 @@
-const AppError = require('../utils/AppError');
+// const AppError = require('../utils/AppError');
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -10,12 +11,9 @@ const sendErrorDev = (err, res) => {
 
 const sendErrorProd = (err, res) => {
   if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
   } else {
-    console.log('ERROR 🙅🏽', err);
+    console.error('ERROR 🙅', err);
+
     res.status(500).json({
       status: 'error',
       message: 'Something went very wrong',
@@ -27,10 +25,9 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'developement') {
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
     sendErrorProd(err, res);
   }
 };
