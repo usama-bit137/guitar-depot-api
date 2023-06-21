170d95138e21fa993f13d0a4b12edfe1d0e4e36c
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-21 23:40:36 +0100

    User controller CRUD HTTP methods implemented for later use.

---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const user = await User.find();
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
+exports.getUser = catchAsync(async (req, res, next) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+});
+
+exports.createUser = catchAsync(async (req, res, next) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+});
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+});
+
+exports.deleteUser = catchAsync(async (req, res, next) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+});
