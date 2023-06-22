cee24a67aacba1bf3f77af4a27e4d2fd091c7b92
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-22 12:55:00 +0100

    Login method added which implements the use of JSONWebTokens to ensure that the user is authorized to login to the account. This links with the password confirmation method in the user model file.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,15 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/AppError');
+
+const signToken = (id) => {
+  // constructs a JSONWebToken from the Secret!
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -9,22 +19,34 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
+  // this generates and sends a JSONWebToken
+  // in the body of the response. Works ✅
+  const token = signToken(newUser._id);
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       newUser,
     },
   });
 });
 
-// exports.login = catchAsync(async (req, res, next) => {
-//   const { email, password } = req.body;
-//   const user = await User.findOne({ email });
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  // so we find the user by the email
 
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       user,
-//     },
-//   });
-// });
+  if (!email || !password)
+    return next(new AppError('Please provide an email and password', 400));
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password)))
+    return new AppError('Incorrect email or password', 401);
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
