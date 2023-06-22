83f8617b2b4c429e54e755167b8fef75cdb13ec8
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-22 12:51:34 +0100

    C:/Program Files/Git/login route implemented.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
-// router.post('/login', authController.login);
+router.post('/login', authController.login);
 
 router.route('/').get(controller.getAllUsers).post(controller.createUser);
 
