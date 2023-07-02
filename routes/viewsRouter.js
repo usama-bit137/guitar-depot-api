129b03f9287a8153287531a687b2e1fd7364da45
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-02 22:17:19 +0100

    slug url implemented which takes you to individual guitar page.

--- routes/viewsRouter.js
+++ routes/viewsRouter.js
@@ -11,7 +11,6 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/overview', controller.getOverview);
-
-router.get('/guitar', controller.getGuitar);
+router.get('/guitar/:slug', controller.getGuitar);
 
 module.exports = router;
