8018416755842e331fc0bc3a90fa426434f55a1e
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-02 18:58:28 +0100

    Router for views created. Imported views handler functions from controller file. Exported to app.js

---
+++ routes/viewsRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const controller = require('../controllers/viewsController');
+const router = express.Router();
+
+// Leave this out of the controller file:
+router.get('/', (req, res, next) => {
+  res.status(200).render('base', {
+    guitar: 'Fender Stratocaster',
+    user: 'Usama',
+  });
+});
+
+router.get('/overview', controller.getOverview);
+
+router.get('/guitar', controller.getGuitar);
+
+module.exports = router;
