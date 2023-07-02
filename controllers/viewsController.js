0983eb135af75253bdf683ff9c6fb17879f9a688
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-02 22:15:57 +0100

    /overview URL serves up guitar overviews. Users can also search a guitar by name by entering the slug in the url (e.g /guitars/<manufacturer>-<Name>).

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,19 @@
-exports.getOverview = (req, res, next) => {
+const Guitar = require('../models/guitarModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const guitars = await Guitar.find();
+
   res.status(200).render('overview', {
     title: 'All Guitars',
+    guitars,
   });
-};
+});
 
-exports.getGuitar = (req, res, next) => {
+exports.getGuitar = catchAsync(async (req, res, next) => {
+  // 1) get the data for the guitar
+  const guitar = await Guitar.findOne({ slug: req.params.slug });
   res.status(200).render('guitar', {
-    title: 'Fender Stratocaster',
+    guitar,
   });
-};
+});
