c177553363ae72f26f54dadff73fe4f5058fb8dc
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-02 18:55:06 +0100

    Views router added.

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const morgan = require('morgan');
 
 const guitarRouter = require('./routes/guitarRoutes');
 const userRouter = require('./routes/userRoutes');
+const viewsRouter = require('./routes/viewsRouter');
+
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 app.set('view engine', 'pug');
-app.set('view', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
@@ -19,14 +21,20 @@ if (process.env.NODE_ENV === 'development') {
   console.log(`You're the ${process.env.NODE_ENV} build`);
 }
 
+// TEMPLATE ROUTES:
+app.use('/', viewsRouter);
+
+//API ROUTES:
 app.use('/api/v1/guitars', guitarRouter);
 app.use('/api/v1/users', userRouter);
 
+// UNHANDLED ROUTES:
 app.use('*', (req, res, next) => {
   // const error = new Error(`Invald URL ${req.originalUrl} on this server.`);
   next(new AppError(`Invald URL ${req.originalUrl} on this server.`, 404));
 });
 
+// ERROR HANDLER MIDDLEWARE
 app.use(globalErrorHandler);
 
 module.exports = app;
